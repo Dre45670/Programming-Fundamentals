f26ed3a3ab8979f59955c6a36d1ad39794bf7727
Andre Logan <75024207+Dre45670@users.noreply.github.com>
2026-05-15 19:50:43 -0400

    Updating lists.py

--- lists.py
+++ lists.py
@@ -11,3 +11,8 @@ availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 print(f"EC2 Instances to terminate: {instance_id}")
 print(f"IP Addresses for security group: {ip_addresses}")
 print(f"Availability Zones: {availability_zones}")
+
+# Add new EC2 instance ID to the list
+instance_id.append("i-3456")
+print ("After adding a new instance ID")
+print(f"EC2 Instances: {instance_id}")
