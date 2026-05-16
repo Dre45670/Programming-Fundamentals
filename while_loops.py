fec785bcda28e3fe0cfbbb55090724e6b0f0e24f
Andre Logan <75024207+Dre45670@users.noreply.github.com>
2026-05-16 18:12:36 -0400

    Updating while_loops.py and while_loops1.py

---
+++ while_loops.py
@@ -0,0 +1,6 @@
+# While loops are used to execute a block of code repeatedly as long as a certain condition is true.
+
+count = 0
+while count < 5:
+    print("Count is:", count)
+    count += 1 # Increment the count to avoid an infinite loop
