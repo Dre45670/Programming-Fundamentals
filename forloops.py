055ae6eedb75a7e878fc3a8bc36089fd2cc16089
Andre Logan <75024207+Dre45670@users.noreply.github.com>
2026-05-16 17:41:35 -0400

    Updating forloops.py and while_loops.py

---
+++ forloops.py
@@ -0,0 +1,10 @@
+# For Loops
+
+instance_ids = ["i-12345abcdef", "i-678990ghijk", "i-abcdef12345"]
+
+for instance_id in instance_ids:
+    print(f"Checking the status of instance {instance_id}")
+    # Code to check instance status
+    print(f"Instance {instance_id} status check is complete.")
+
+print("All instance status checks are complete.")
