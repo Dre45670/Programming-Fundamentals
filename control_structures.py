c2b1cec1438219405cc5e143b69865dd7259a4a2
Andre Logan <75024207+Dre45670@users.noreply.github.com>
2026-05-16 17:10:29 -0400

    Updating control structures.py and adding forloops.py

--- control_structures.py
+++ control_structures.py
@@ -1,9 +1,20 @@
 # Control Structures / Control Flow
+#instance_running = "broken"
 
 # If Statements
-if condition:
-    # Code to execute if condition is True
-elif another_condition:
-    # Code to execute if another_condition is True
+#if instance_running == "running":
+#    print("The EC2 is running.")
+#elif instance_running == "stopped":
+#    print("The EC2 is stopped.....")
+#else:
+#    print("The EC2 is in an unknown state.")
+
+public_access_block = "true"
+
+# Write the If Statement
+#If condition
+if public_access_block == "false":
+    print("The S3 bucket is secure.")
+#Else condition
 else:
-    # Code to execute if all conditions are False
+    print("The S3 bucket is not secure.")
